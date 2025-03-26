6f7ee997d536c79b88ae2ad341bdfbe45bd0b4b7
Minsoo Kang <alstn38@naver.com>
2025-03-26 23:41:02 +0900

    init: Stepic Project 생성

---
+++ Stepic/Stepic/Present/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Stepic
+//
+//  Created by 강민수 on 3/26/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .cyan
+    }
+}
