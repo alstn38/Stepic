fc74b160fd97c218684faa0a224bdbbb5ce88a23
Minsoo Kang <alstn38@naver.com>
2025-03-27 20:47:59 +0900

    feat: UIIMage+Literals Image 추가

---
+++ Stepic/Stepic/Resource/Extension/UIImage+Literals.swift
@@ -0,0 +1,19 @@
+//
+//  UIImage+Literals.swift
+//  Stepic
+//
+//  Created by 강민수 on 3/27/25.
+//
+
+import UIKit
+
+extension UIImage {
+    
+    static var homeImage: UIImage {
+        return UIImage(systemName: "house")!
+    }
+    
+    static var personImage: UIImage {
+        return UIImage(systemName: "person")!
+    }
+}
