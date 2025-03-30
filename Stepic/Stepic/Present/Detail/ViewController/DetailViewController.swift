98498f949f3e0136ed076a9ef2134d695b4bcc1b
Minsoo Kang <alstn38@naver.com>
2025-03-30 23:21:15 +0900

    feat: DetailViewController Record View 추가 및 키보드 대응

--- Stepic/Stepic/Present/Detail/ViewController/DetailViewController.swift
+++ Stepic/Stepic/Present/Detail/ViewController/DetailViewController.swift
@@ -7,16 +7,25 @@
 
 import UIKit
 
+import RxCocoa
+import RxGesture
+import RxSwift
 import SnapKit
 
 final class DetailViewController: UIViewController {
     
+    private let disposeBag = DisposeBag()
+    
     private let bookMarkButton = UIBarButtonItem()
+    private let scrollView = UIScrollView()
+    private let contentView = UIView()
     private let walkInfoView = WalkInfoView()
+    private let recordView = RecordView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        configureBind()
         configureNavigation()
         configureView()
         configureHierarchy()
@@ -30,20 +39,56 @@ final class DetailViewController: UIViewController {
         navigationItem.rightBarButtonItem = bookMarkButton
     }
     
+    private func configureBind() {
+        scrollView.rx.didScroll
+            .bind(with: self) { owner, _ in
+                owner.view.endEditing(true)
+            }
+            .disposed(by: disposeBag)
+        
+        contentView.rx.tapGesture()
+            .bind(with: self) { owner, _ in
+                owner.view.endEditing(true)
+            }
+            .disposed(by: disposeBag)
+    }
+    
     private func configureView() {
         view.backgroundColor = .backgroundPrimary
+        
+        scrollView.showsVerticalScrollIndicator = false
     }
     
     private func configureHierarchy() {
-        view.addSubviews(
-            walkInfoView
+        view.addSubview(scrollView)
+        
+        scrollView.addSubview(contentView)
+        
+        contentView.addSubviews(
+            walkInfoView,
+            recordView
         )
     }
     
     private func configureLayout() {
+        scrollView.snp.makeConstraints {
+            $0.edges.equalTo(view.safeAreaLayoutGuide)
+        }
+        
+        contentView.snp.makeConstraints {
+            $0.edges.equalTo(scrollView)
+            $0.width.equalTo(scrollView)
+        }
+        
         walkInfoView.snp.makeConstraints {
-            $0.top.equalTo(view.safeAreaLayoutGuide).offset(20)
+            $0.top.equalToSuperview().offset(20)
             $0.horizontalEdges.equalToSuperview()
+        }
+        
+        recordView.snp.makeConstraints {
+            $0.top.equalTo(walkInfoView.snp.bottom).offset(34)
+            $0.horizontalEdges.equalToSuperview()
+            $0.bottom.equalToSuperview() // 항상 마지막 뷰는 해당 레이아웃 추가
         }
     }
 }
