2258cdc9c2bbe0381982d4bf71ef6f3bfc834c88
Minsoo Kang <alstn38@naver.com>
2025-03-31 23:25:44 +0900

    feat: MyPageViewController InfoView Layout 설정

--- Stepic/Stepic/Present/MyPage/ViewController/MyPageViewController.swift
+++ Stepic/Stepic/Present/MyPage/ViewController/MyPageViewController.swift
@@ -7,10 +7,43 @@
 
 import UIKit
 
+import SnapKit
+
 final class MyPageViewController: UIViewController {
+    
+    private let settingButton = UIBarButtonItem()
+    private let myPageInfoView = MyPageInfoView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .cyan
+        
+        configureNavigation()
+        configureView()
+        configureHierarchy()
+        configureLayout()
+    }
+    
+    private func configureNavigation() {
+        settingButton.image = .gearShape
+        settingButton.tintColor = .textPrimary
+        
+        navigationItem.rightBarButtonItem = settingButton
+    }
+    
+    private func configureView() {
+        view.backgroundColor = .backgroundPrimary
+    }
+    
+    private func configureHierarchy() {
+        view.addSubviews(
+            myPageInfoView
+        )
+    }
+    
+    private func configureLayout() {
+        myPageInfoView.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide)
+            $0.horizontalEdges.equalToSuperview()
+        }
     }
 }
