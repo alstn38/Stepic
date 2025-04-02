83c182dd710e75581cb3311c407b6cefbff09acc
Minsoo Kang <alstn38@naver.com>
2025-04-02 21:04:38 +0900

    feat: WalkTrackingEntity Model 정의

---
+++ Stepic/Stepic/Domain/Entity/WalkTrackingEntity.swift
@@ -0,0 +1,19 @@
+//
+//  WalkTrackingEntity.swift
+//  Stepic
+//
+//  Created by 강민수 on 4/2/25.
+//
+
+import Foundation
+
+import CoreLocation
+
+struct WalkTrackingEntity {
+    let startTime: Date
+    let endTime: Date
+    let duration: TimeInterval
+    let distance: Double
+    let pathCoordinates: [CLLocationCoordinate2D]
+    let startDate: Date
+}
