a978b86f9ea560c669b32b4933988ffdd904b42a
Minsoo Kang <alstn38@naver.com>
2025-04-02 16:17:24 +0900

    feat: CurrentWeatherDTO Model 생성

---
+++ Stepic/Stepic/Data/DTO/Weather/CurrentWeatherDTO.swift
@@ -0,0 +1,15 @@
+//
+//  CurrentWeatherDTO.swift
+//  Stepic
+//
+//  Created by 강민수 on 4/1/25.
+//
+
+import Foundation
+
+struct CurrentWeatherDTO {
+    let symbolName: String
+    let description: String
+    let temperature: Double
+    let unitSymbol: String
+}
