5aa2fd86e99fe3d8e5fc7a7ee14a88e1723edc05
Minsoo Kang <alstn38@naver.com>
2025-04-03 22:14:18 +0900

    feat: DetailView Photo Select 기능 구현

--- Stepic/Stepic/Present/Detail/ViewController/DetailViewController.swift
+++ Stepic/Stepic/Present/Detail/ViewController/DetailViewController.swift
@@ -8,14 +8,63 @@
 import UIKit
 
 import RxCocoa
+import RxDataSources
 import RxGesture
 import RxSwift
 import SnapKit
+import YPImagePicker
 
 final class DetailViewController: UIViewController {
     
     private let viewModel: DetailViewModel
+    private let photoDidDeleteRelay = PublishRelay<Int>()
+    private let cameraActionDidTap = PublishRelay<Void>()
+    private let libraryActionDidTap = PublishRelay<Void>()
     private let disposeBag = DisposeBag()
+    
+    private lazy var dataSource = RxCollectionViewSectionedReloadDataSource<DetailPhotoSection>(
+        configureCell: { [weak self] _, collectionView, indexPath, item in
+            switch item {
+            case .photo(let data):
+                guard let cell = collectionView.dequeueReusableCell(
+                    withReuseIdentifier: DetailPictureCollectionViewCell.identifier,
+                    for: indexPath
+                ) as? DetailPictureCollectionViewCell else { return UICollectionViewCell() }
+                
+                cell.configureView(data)
+                cell.deleteButton.rx.tap
+                    .map { indexPath.item }
+                    .bind { [weak self] index in
+                        self?.photoDidDeleteRelay.accept(index)
+                    }
+                    .disposed(by: cell.disposeBag)
+                
+                return cell
+
+            case .addPlaceholder:
+                guard let cell = collectionView.dequeueReusableCell(
+                    withReuseIdentifier: DetailAddPictureCollectionViewCell.identifier,
+                    for: indexPath
+                ) as? DetailAddPictureCollectionViewCell else { return UICollectionViewCell() }
+                cell.addPictureButton.rx.tap
+                    .bind { [weak self] _ in
+                        self?.presentActionSheet()
+                    }
+                    .disposed(by: cell.disposeBag)
+                
+                return cell
+            }
+        }
+    )
+    
+    private let bookMarkButton = UIBarButtonItem()
+    private let scrollView = UIScrollView()
+    private let contentView = UIView()
+    private let walkInfoView = WalkInfoView()
+    private let pictureSelectView = PictureSelectView()
+    private let recordView = RecordView()
+    private let routeView = RouteView()
+    private let recordButton = UIButton(type: .system)
     
     init(viewModel: DetailViewModel) {
         self.viewModel = viewModel
@@ -26,15 +75,6 @@ final class DetailViewController: UIViewController {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
-    private let bookMarkButton = UIBarButtonItem()
-    private let scrollView = UIScrollView()
-    private let contentView = UIView()
-    private let walkInfoView = WalkInfoView()
-    private let pictureSelectView = PictureSelectView()
-    private let recordView = RecordView()
-    private let routeView = RouteView()
-    private let recordButton = UIButton(type: .system)
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -47,12 +87,38 @@ final class DetailViewController: UIViewController {
     }
     
     private func configureBind() {
-        let input = DetailViewModel.Input()
+        let didAddPhotoRelay = PublishRelay<[WalkPhotoEntity]>()
+        
+        let input = DetailViewModel.Input(
+            photoDidDelete: photoDidDeleteRelay.asObservable(),
+            photoDidAdd: didAddPhotoRelay.asObservable(),
+            cameraActionDidTap: cameraActionDidTap.asObservable(),
+            libraryActionDidTap: libraryActionDidTap.asObservable()
+        )
         let output = viewModel.transform(from: input)
         
         output.walkResultData
             .drive(with: self) { owner, data in
                 owner.walkInfoView.configureView(data)
+            }
+            .disposed(by: disposeBag)
+        
+        output.photoData
+            .map { data in
+                var items = data.map { DetailPhotoItem.photo($0) }
+                if items.count < 10 {
+                    items.append(.addPlaceholder)
+                }
+                return [DetailPhotoSection(items: items)]
+            }
+            .drive(pictureSelectView.pictureCollectionView.rx.items(dataSource: dataSource))
+            .disposed(by: disposeBag)
+        
+        output.presentPickerView
+            .drive(with: self) { owner, source in
+                owner.presentImagePicker(source: source) { selectPhotos in
+                    didAddPhotoRelay.accept(selectPhotos)
+                }
             }
             .disposed(by: disposeBag)
         
@@ -136,6 +202,79 @@ final class DetailViewController: UIViewController {
             $0.height.equalTo(44)
             $0.bottom.equalToSuperview().inset(54)
         }
+    }
+    
+    private func presentActionSheet() {
+        let alert = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+
+        let cameraAction = UIAlertAction(
+            title: .StringLiterals.Alert.photoActionCamera,
+            style: .default
+        ) { [weak self] _ in
+            self?.cameraActionDidTap.accept(())
+        }
+        
+        let libraryAction = UIAlertAction(
+            title: .StringLiterals.Alert.photoActionLibrary,
+            style: .default
+        ) { [weak self] _ in
+            self?.libraryActionDidTap.accept(())
+        }
+
+        let cancelAction = UIAlertAction(
+            title: .StringLiterals.Alert.locationAlertCancel,
+            style: .cancel
+        )
+
+        alert.addAction(cameraAction)
+        alert.addAction(libraryAction)
+        alert.addAction(cancelAction)
+
+        present(alert, animated: true)
+    }
+    
+    private func presentImagePicker(source: DetailViewModel.ImagePickerSource, onResult: @escaping ([WalkPhotoEntity]) -> Void) {
+        var config = YPImagePickerConfiguration()
+        config.library.mediaType = .photo
+        config.showsPhotoFilters = false
+        config.library.preSelectItemOnMultipleSelection = false
+        config.library.defaultMultipleSelection = true
+        config.onlySquareImagesFromCamera = false
+        
+        switch source {
+        case .library(let maxCount):
+            config.library.maxNumberOfItems = maxCount
+            config.startOnScreen = .library
+            config.screens = [.library]
+        case .camera(let maxCount):
+            config.library.maxNumberOfItems = maxCount
+            config.startOnScreen = .photo
+            config.screens = [.photo]
+        }
+        
+        let picker = YPImagePicker(configuration: config)
+        
+        picker.didFinishPicking { items, _ in
+            var walkPhotos: [WalkPhotoEntity] = []
+            
+            for item in items {
+                switch item {
+                case .photo(let photo):
+                    let image = photo.image
+                    let location = photo.asset?.location
+                    
+                    walkPhotos.append(WalkPhotoEntity(image: image, location: location))
+                    
+                default:
+                    break
+                }
+            }
+            
+            onResult(walkPhotos)
+            picker.dismiss(animated: true)
+        }
+        
+        self.present(picker, animated: true)
     }
     
     private func scrollToRecordView() {
--- Stepic/Stepic/Present/Detail/ViewModel/DetailViewModel.swift
+++ Stepic/Stepic/Present/Detail/ViewModel/DetailViewModel.swift
@@ -13,22 +13,76 @@ import RxCocoa
 final class DetailViewModel: InputOutputModel {
     
     struct Input {
-        
+        let photoDidDelete: Observable<Int>
+        let photoDidAdd: Observable<[WalkPhotoEntity]>
+        let cameraActionDidTap: Observable<Void>
+        let libraryActionDidTap: Observable<Void>
     }
     
     struct Output {
         let walkResultData: Driver<WalkResultEntity>
+        let photoData: Driver<[WalkPhotoEntity]>
+        let presentPickerView: Driver<ImagePickerSource>
     }
     
+    private let maxPhotoCount: Int = 10
     private let walkResultData: WalkResultEntity
+    private let photoDataRelay: BehaviorRelay<[WalkPhotoEntity]>
+    private let disposeBag = DisposeBag()
     
-    init(walkResultData: WalkResultEntity) {
+    init(walkResultData: WalkResultEntity, walkPhotoData: [WalkPhotoEntity]) {
         self.walkResultData = walkResultData
+        self.photoDataRelay = BehaviorRelay(value: walkPhotoData)
     }
     
     func transform(from input: Input) -> Output {
-        let walkResultDataRelay = BehaviorRelay(value: walkResultData)
+        let presentPickerViewRelay = PublishRelay<ImagePickerSource>()
         
-        return Output(walkResultData: Observable.just(walkResultData).asDriver(onErrorDriveWith: .empty()))
+        input.photoDidDelete
+            .bind(with: self) { owner, index in
+                var newData = owner.photoDataRelay.value
+                newData.remove(at: index)
+                owner.photoDataRelay.accept(newData)
+            }
+            .disposed(by: disposeBag)
+        
+        input.photoDidAdd
+            .bind(with: self) { owner, inputPhotos in
+                let newPhotoData = owner.photoDataRelay.value + inputPhotos
+                owner.photoDataRelay.accept(newPhotoData)
+            }
+            .disposed(by: disposeBag)
+        
+        input.cameraActionDidTap
+            .withUnretained(self)
+            .map { $0.0.maxPhotoCount - $0.0.photoDataRelay.value.count }
+            .bind { possibleCount in
+                presentPickerViewRelay.accept(.camera(maxCount: possibleCount))
+            }
+            .disposed(by: disposeBag)
+        
+        input.libraryActionDidTap
+            .withUnretained(self)
+            .map { $0.0.maxPhotoCount - $0.0.photoDataRelay.value.count }
+            .bind { possibleCount in
+                presentPickerViewRelay.accept(.library(maxCount: possibleCount))
+            }
+            .disposed(by: disposeBag)
+        
+        
+        return Output(
+            walkResultData: Observable.just(walkResultData).asDriver(onErrorDriveWith: .empty()),
+            photoData: photoDataRelay.asDriver(),
+            presentPickerView: presentPickerViewRelay.asDriver(onErrorDriveWith: .empty()),
+        )
+    }
+}
+
+// MARK: - ImagePickerSource
+extension DetailViewModel {
+    
+    enum ImagePickerSource {
+        case library(maxCount: Int)
+        case camera(maxCount: Int)
     }
 }
