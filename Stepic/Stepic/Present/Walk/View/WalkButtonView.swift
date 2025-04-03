c10d5fff938176d728a65684cca16c33cb925313
Minsoo Kang <alstn38@naver.com>
2025-04-03 18:05:26 +0900

    fix: WalkButtonView Toast 시점 수정

--- Stepic/Stepic/Present/Walk/View/WalkButtonView.swift
+++ Stepic/Stepic/Present/Walk/View/WalkButtonView.swift
@@ -108,8 +108,10 @@ final class WalkButtonView: UIView {
         switch gesture.state {
         case .began:
             startLongPressAnimation()
-        case .ended, .cancelled:
+        case .cancelled:
             AlertToastManager.showToastAtPresentedView(message: .StringLiterals.Toast.walkFinishHoldMessage)
+            resetButton()
+        case .ended, .cancelled:
             resetButton()
         default:
             break
