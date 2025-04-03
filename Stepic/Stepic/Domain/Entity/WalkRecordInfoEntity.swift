5505b8a7eda3fe41cf14ed1f926998795938c3bc
Minsoo Kang <alstn38@naver.com>
2025-04-03 22:44:19 +0900

    feat: WalkRecordInfoEntity Model 정의

---
+++ Stepic/Stepic/Domain/Entity/WalkRecordInfoEntity.swift
@@ -0,0 +1,14 @@
+//
+//  WalkRecordInfoEntity.swift
+//  Stepic
+//
+//  Created by 강민수 on 4/3/25.
+//
+
+import Foundation
+
+struct WalkRecordInfoEntity {
+    var emotion: EmotionTypeEntity?
+    var title: String = ""
+    var content: String = ""
+}
