b901ee2e4cdf96fc38147bab5be7805a23197510
Minsoo Kang <alstn38@naver.com>
2025-04-23 16:23:52 +0900

    feat: StepicWidget Widget 크기별 추가

--- Stepic/StepicWidget/Widget/StepicWidget/StepicWidget.swift
+++ Stepic/StepicWidget/Widget/StepicWidget/StepicWidget.swift
@@ -9,21 +9,22 @@ import WidgetKit
 import SwiftUI
 
 struct StepicWidget: Widget {
+    
     let kind: String = "StepicWidget"
 
     var body: some WidgetConfiguration {
         StaticConfiguration(kind: kind, provider: WidgetCalendarTimelineProvider()) { entry in
             if #available(iOS 17.0, *) {
-                WeeklyCalendarWidgetView(entry: entry)
+                StepicWidgetEntryView(entry: entry)
                     .containerBackground(.fill.tertiary, for: .widget)
             } else {
-                WeeklyCalendarWidgetView(entry: entry)
+                StepicWidgetEntryView(entry: entry)
                     .padding()
                     .background()
             }
         }
-        .supportedFamilies([.systemMedium])
-        .configurationDisplayName("이번 주 감정 캘린더")
-        .description("최근 일주일 동안의 감정 기록을 캘린더 형태로 보여줍니다.")
+        .supportedFamilies([.systemSmall, .systemMedium, .systemLarge])
+        .configurationDisplayName(String.StringLiterals.Widget.widgetDisplayName)
+        .description(String.StringLiterals.Widget.widgetDescription)
     }
 }
