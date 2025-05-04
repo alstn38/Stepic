079df54e4208825ae7d005fedcba030168beeae6
Minsoo Kang <alstn38@naver.com>
2025-05-04 13:31:39 +0900

    chore: KalmanFilterManager 주석 추가

--- Stepic/Stepic/Domain/Manager/KalmanFilterManager.swift
+++ Stepic/Stepic/Domain/Manager/KalmanFilterManager.swift
@@ -9,15 +9,15 @@ import Foundation
 import CoreLocation
 
 final class KalmanFilterManager {
-    // 프로세스 노이즈 (시스템의 불확실성, 작을수록 추정값에 더 의존)
+    /// 프로세스 노이즈 (시스템의 불확실성, 작을수록 추정값에 더 의존)
     var Q: Double
-    // 측정 노이즈 (센서의 노이즈 수준, 작을수록 센서 값을 신뢰)
+    /// 측정 노이즈 (센서의 노이즈 수준, 작을수록 센서 값을 신뢰)
     var R: Double
-    // 현재 추정값
+    /// 현재 추정값
     var x: Double
-    // 오차 공분산
+    /// 오차 공분산
     var p: Double
-    // 칼만 게인 (업데이트 단계에서 사용)
+    /// 칼만 게인 (업데이트 단계에서 사용)
     var K: Double = 0.0
 
     /// 초기값, 초기 오차, 프로세스 노이즈, 측정 노이즈를 설정하여 칼만 필터를 초기화합니다.
@@ -52,13 +52,11 @@ final class KalmanFilterManager {
     func update(measurement: Double, accuracy: CLLocationAccuracy, speed: CLLocationSpeed) -> Double {
         updateR(basedOn: accuracy)
         updateQ(basedOn: speed)
-        print("speed = \(speed)")
-        print("accuracy = \(accuracy)")
         
-        // 예측 단계
+        /// 예측 단계
         p = p + Q
         
-        // 업데이트 단계
+        /// 업데이트 단계
         K = p / (p + R)
         x = x + K * (measurement - x)
         p = (1 - K) * p
@@ -70,11 +68,11 @@ final class KalmanFilterManager {
     private func updateR(basedOn accuracy: CLLocationAccuracy) {
         switch accuracy {
         case ..<5:
-            R = 0.01  // 매우 정확할 때는 센서 값을 강하게 반영
+            R = 0.01  /// 매우 정확할 때는 센서 값을 강하게 반영
         case 5..<10:
             R = 0.05
         default:
-            R = 0.1   // 정확도가 낮을수록 센서 신뢰도 낮춤
+            R = 0.1   /// 정확도가 낮을수록 센서 신뢰도 낮춤
         }
     }
 
@@ -82,13 +80,13 @@ final class KalmanFilterManager {
     private func updateQ(basedOn speed: CLLocationSpeed) {
         switch speed {
         case ..<1.0:
-            Q = 0.005  // 정지 상태에 가까움 → 변화 거의 없음
+            Q = 0.005  /// 정지 상태에 가까움
         case 1.0..<3.0:
-            Q = 0.01   // 일반적인 걷기
+            Q = 0.01   /// 일반적인 걷기
         case 3.0..<6.0:
-            Q = 0.02   // 빠르게 걷기 또는 조깅
+            Q = 0.02   /// 빠르게 걷기 또는 조깅
         default:
-            Q = 0.05   // 달리기 이상 → 추정에 더 민감하게 반응
+            Q = 0.05   /// 달리기 이상
         }
     }
 }
